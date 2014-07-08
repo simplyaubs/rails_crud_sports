ce3ad2e7c50daa888d5c868b683ab3188d90ae3d
Aubrey Howell <simplyaubs@gmail.com>
2014-07-08 15:56:10 -0600

    User can update a sport from list.

--- app/controllers/sports_controller.rb
+++ app/controllers/sports_controller.rb
@@ -15,6 +15,21 @@ class SportsController < ApplicationController
     end
   end
 
+  def show
+    @sport = Sport.find(params[:id])
+  end
+
+  def edit
+    @sport = Sport.find(params[:id])
+  end
+
+  def update
+    @sport = Sport.find(params[:id])
+    @sport.update_attributes!(sport_params)
+
+    redirect_to sports_path
+  end
+
   private
   def sport_params
     params.require(:sport).permit(:name, :equip)
--- spec/features/sports_spec.rb
+++ spec/features/sports_spec.rb
@@ -11,4 +11,26 @@ feature 'CRUD sports' do
     expect(page).to have_content 'Soccer'
     expect(page).to have_content 'Shin guards'
   end
+
+  scenario 'User can update a sport from list' do
+    visit '/'
+    expect(page).to have_content 'Welcome'
+    click_on 'Add a sport'
+    fill_in 'Name', with: 'Soccer'
+    fill_in 'Equip', with: 'Shin guards'
+    click_on 'Add sport'
+    expect(page).to have_content 'Soccer'
+    expect(page).to have_content 'Shin guards'
+    click_on 'Soccer'
+    expect(page).to have_content 'Soccer'
+    expect(page).to have_content 'Shin guards'
+    click_on 'Edit'
+    fill_in 'Name', with: 'Softball'
+    fill_in 'Equip', with: 'Cleats'
+    click_on 'Update sport'
+    expect(page).to have_content 'Softball'
+    expect(page).to have_content 'Cleats'
+    expect(page).to_not have_content 'Soccer'
+    expect(page).to_not have_content 'Shin guards'
+  end
 end
